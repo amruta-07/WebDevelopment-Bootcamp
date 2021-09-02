133dd89e4073eb3c2ddfc35c4b86e48d75076b57
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-09-02 23:21:53 +0530

    Add files via upload

---
+++ JS Tutorial/tut1.js
@@ -0,0 +1,30 @@
+console.log("tut1");
+//variable in js
+//var,let,const
+var name = "Amruta";
+var channel;
+var marks= 8954;
+channel="CodeWithMe"
+console.log(name, channel,marks); 
+/* 1.can start with Numbers,letters,_ or $
+ 2. js are case sensitive */
+ var city="Delhi";
+ console.log(city);
+
+
+ {
+  let city= "Rampur";
+  city = "Kolkata";
+  console.log(city);
+ }
+ console.log(city)
+
+ const arr1 = [23,45,63,6];
+ //arr1.push(45);
+ console.log(arr1);
+
+ const arr2 = [5,6,8];
+ arr2.push(9);
+ console.log(arr2);
+  
+
