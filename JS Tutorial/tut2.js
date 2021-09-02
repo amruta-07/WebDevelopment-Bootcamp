e21294f5c086ef134ecbb083cabdb351370c1b4b
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-09-02 23:22:45 +0530

    Add files via upload

---
+++ JS Tutorial/tut2.js
@@ -0,0 +1,33 @@
+// Primitive data types
+// String
+let name = "Amruta";
+console.log("My string is:" + name);
+console.log("Data type is:"+ (typeof name));
+
+// Number
+let marks = 34;
+console.log("Data type is:"+ (typeof marks));
+
+// Boolean
+let isDriver= true;
+console.log("Data type is:"+ (typeof isDriver));
+
+// null
+let nullVar = null;
+console.log("Data type is:"+ (typeof nullVar));
+
+// reference data types
+
+// arrays
+let myarr = [2,4,6,7, false,"string"];
+console.log("Data type is:"+ (typeof myarr));
+
+
+
+
+
+    
+
+
+
+
