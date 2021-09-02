663dbf8bbb095f05999db8e62dfdb49af5611b1b
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-09-02 23:20:28 +0530

    Add files via upload

---
+++ JS Tutorial/index.js
@@ -0,0 +1,13 @@
+console.time("Your code took");
+console.log("Hello World");
+console.table({Amruta: 'this',marks:34})
+// consol.log and consol.table will be removed after console.clear()
+// console.clear() is used for console clear
+//console.clear()
+console.log(43+32);
+console.table({Amruta: 'this',marks:34})
+console.log([55,4,5,6])
+console.warn("This is warning")
+console.log({amruta:'this',marks:62})
+console.timeEnd("Your code took");
+//console.error("This is an Error");
