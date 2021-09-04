9eed40e272e57bff4c3104fb9ec12d7c6b7a2ffa
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-09-04 23:21:20 +0530

    Add files via upload

---
+++ JS Tutorial/tut5.js
@@ -0,0 +1,32 @@
+console.log("welcome to tut5.js");
+const marks = [34,23,24,45,67];
+const mixed = ['str',45,96,[3,5]];
+const arr = new Array(23,123,21,'Orange'); 
+const fruits = ['Orange','Apple','Orange','Pineapple'];
+console.log(arr);
+console.log(mixed);
+console.log(fruits[3]);
+console.log(marks);
+console.log(arr.length);
+
+
+arr[0] = 'Amruta';
+let arrelement = arr[0];
+console.log(arr);
+
+//let value = marks.indexOf(87);
+let value = marks.indexOf(67);
+console.log(value);
+
+//Mutating and modifying arrays
+marks.push(558569);
+console.log(marks);
+
+
+
+
+
+
+
+
+
