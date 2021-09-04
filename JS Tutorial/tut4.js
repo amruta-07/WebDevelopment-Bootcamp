56b1390ceb2f538244aadfff4dae5030475c3554
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-09-04 23:20:20 +0530

    Add files via upload

---
+++ JS Tutorial/tut4.js
@@ -0,0 +1,30 @@
+console.log("welcom to tut4");
+const name = 'Amruta';
+const greeting = 'Good Morning'; 
+//console.log(greeting + ' ' +  name);
+
+let html;
+html = "<h1>This is my Tutorial</h1>" + "<>This is My paragraph</>";
+html = html.concat('This',' str2');
+//console.log(html);
+// console.log(html.length);
+// console.log(html.toLowerCase());
+// console.log(html.toUpperCase());
+// console.log(html);
+
+//console.log(html[7])
+console.log(html.indexOf('is'))
+console.log(html.lastIndexOf('>'))
+console.log(html.charAt(12))
+console.log(html.endsWith('fdefss'));
+console.log(html.includes(' paragraph'))
+console.log(html.substring(1,6));
+console.log(html.split(''));
+
+
+
+
+
+
+
+
