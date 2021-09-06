eea25cd71494437b8b96f24a4c6e5efee3106512
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-09-06 22:38:12 +0530

    Add files via upload

--- JS Tutorial/tut6.js
+++ JS Tutorial/tut6.js
@@ -1,1 +1,65 @@
 console.log("Welcome to tut6.js");
+// const age = 39;
+// if(age==19)
+// {
+//  console.log("my age is 19")
+// }
+// else if(age == 65){
+//     console.log("age is 65")
+// }
+// else{
+// console.log("my age is not 19")
+// } 
+const age = 128;
+const doesDrive = false;
+// const vari = 34;
+
+// if (age!=19){
+//     console.log('Age is not 19')
+// }
+
+// if(age !== 65){
+//     console.log('Age is not 65')
+// }
+
+// else{
+//     console.log('Age is not 19')
+// }
+
+// if (typeof vari !== 'undefined'){
+//     console.log('Vari is defined');
+// }
+
+// else{
+//     console.log('Vari is not defined');
+// }
+
+// if (doesDrive || age>18){
+//     console.log("You can drive");
+// }
+// else{
+//     console.log("You cannot drive");
+// }
+
+// console.log(age==45? 'Age is 45': 'Age is not 45');
+
+switch (age) {
+    case 18:
+        console.log("You are 18");
+        break;
+
+    case 28:
+        console.log("You are 28");
+        break;
+
+    case 38:
+        console.log("You are 38");
+        break;
+
+    default:
+        console.log("You are unknown age");
+        break;
+}         
+
+ 
+
