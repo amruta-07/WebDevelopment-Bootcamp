d0498342741fbf4b2c37838857a143e30dad9521
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-09-08 13:33:30 +0530

    Add files via upload

---
+++ JS Tutorial/tut7.js
@@ -0,0 +1,55 @@
+console.log("welcome to tut7.js");
+//General Loops:
+// let a = 34;
+// a += 1;
+// a++;
+// console.log(a);
+// for(let i= 0;i<100; i++)
+// {
+//  console.log(i);
+// }
+// let j = 0;
+// while(j<20)
+// {
+//     console.log(j+1);
+//     j+=1;
+
+// }
+// let k = 0;
+// do{
+//     if(k===5){
+//         k+=1;
+//         continue;
+//     }
+//     console.log(k+1);
+//     k+=1;
+// }
+// while(k<10)
+// console.log('done');
+
+let arr=[7,4,8,3,9,1,2];
+arr.forEach(function(element){
+    console.log(element)
+});
+for(let i=0;i<arr.length;i++)
+{
+    const element=arr[i];
+    console.log(element);
+}
+
+// let obj = {
+//     name: "Amruta Misal",
+//     age: 21,
+//     type: "Dangerous programmer",
+//     os: "Ubuntu"
+// }
+// for(let key in obj)
+// {
+//     console.log(`${key} of object is ${Obj[key]}`)
+
+// }
+// console.log('done');
+
+
+
+
