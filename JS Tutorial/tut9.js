a64a2d53b5b5b52f3066a3a38463b2a4a4b152bf
Amruta Misal <79842525+amruta-07@users.noreply.github.com>
2021-09-12 23:25:08 +0530

    Add files via upload

---
+++ JS Tutorial/tut9.js
@@ -0,0 +1,13 @@
+consol.log("Welcome to tu9.js");
+let a = window.document;
+// alert('hello harry');
+// a = prompt('This will destroy your computer. Type your name');
+
+// a = confirm('Are you sure you want to delete this page?');
+a = window.innerHeight;
+a = innerWidth;
+a = scrollY;
+a = location.toString();
+a = window.history;
+console.log(a);          
+
